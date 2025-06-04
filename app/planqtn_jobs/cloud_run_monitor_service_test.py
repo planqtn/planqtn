4165a073e635a34c6dfe32c9814523791b81a039
Balint Pato <patobalint@gmail.com>
2025-06-04 12:50:11 -0400

    fix test paths

--- app/planqtn_jobs/cloud_run_monitor_service_test.py
+++ app/planqtn_jobs/cloud_run_monitor_service_test.py
@@ -4,7 +4,7 @@ from cloud_run_monitor_service import extract_details, extract_details_from_deco
 
 def test_extract_job_id():
     decoded_data = open(
-        "planqtn_jobs/test_files/failed_job_example_message.json", "r"
+        "app/planqtn_jobs/test_files/failed_job_example_message.json", "r"
     ).read()
     uuid, user_id, result = extract_details_from_decoded_data(decoded_data)
     assert uuid == "12345"
@@ -16,7 +16,7 @@ def test_extract_job_id():
 
 
 def test_extract_data_from_failure_message():
-    decoded_data = open("planqtn_jobs/test_files/failure_msg.json", "r").read()
+    decoded_data = open("app/planqtn_jobs/test_files/failure_msg.json", "r").read()
     uuid, user_id, result = extract_details(json.loads(decoded_data))
     assert uuid == "df5688de-0ad4-4cc3-83a0-f0c6a21577fc"
     assert user_id == "95f5b295-530f-42f1-98b1-76f2ce2ba37b"
