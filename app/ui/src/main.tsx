db3c243d0402fbafeaea10a9ad5c809b7f4d869d
Balint Pato <patobalint@gmail.com>
2025-06-10 15:52:02 -0400

    down + teaser pages, controlled by env vars

--- app/ui/src/main.tsx
+++ app/ui/src/main.tsx
@@ -7,16 +7,22 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import LegoStudioView from "./LegoStudio";
 import ErrorBoundary from "./components/ErrorBoundary";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <ErrorBoundary>
-      <ChakraProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<LegoStudioView />} />
-          </Routes>
-        </BrowserRouter>
-      </ChakraProvider>
-    </ErrorBoundary>
-  </React.StrictMode>
-);
+if (import.meta.env.VITE_ENV === "TEASER") {
+  window.location.href = "/teaser.html";
+} else if (import.meta.env.VITE_ENV === "DOWN") {
+  window.location.href = "/down.html";
+} else {
+  ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+      <ErrorBoundary>
+        <ChakraProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<LegoStudioView />} />
+            </Routes>
+          </BrowserRouter>
+        </ChakraProvider>
+      </ErrorBoundary>
+    </React.StrictMode>
+  );
+}
