f24c05dc58810e9f0ea5610dd3d9b267b7b4b71d
Balint Pato <patobalint@gmail.com>
2025-06-26 15:10:11 -0400

    autoflow uses stores now

---
+++ app/ui/src/stores/tensorNetworkStore.ts
@@ -0,0 +1,40 @@
+import { create } from "zustand";
+import { TensorNetwork } from "../lib/TensorNetwork";
+
+interface TensorNetworkStore {
+  tensorNetwork: TensorNetwork | null;
+  setTensorNetwork: (
+    networkOrUpdater:
+      | TensorNetwork
+      | null
+      | ((prev: TensorNetwork | null) => TensorNetwork | null)
+  ) => void;
+  updateTensorNetwork: (
+    updater: (prev: TensorNetwork | null) => TensorNetwork | null
+  ) => void;
+}
+
+export const useTensorNetworkStore = create<TensorNetworkStore>((set) => ({
+  tensorNetwork: null,
+
+  setTensorNetwork: (
+    networkOrUpdater:
+      | TensorNetwork
+      | null
+      | ((prev: TensorNetwork | null) => TensorNetwork | null)
+  ) => {
+    if (typeof networkOrUpdater === "function") {
+      set((state) => ({
+        tensorNetwork: networkOrUpdater(state.tensorNetwork)
+      }));
+    } else {
+      set({ tensorNetwork: networkOrUpdater });
+    }
+  },
+
+  updateTensorNetwork: (
+    updater: (prev: TensorNetwork | null) => TensorNetwork | null
+  ) => {
+    set((state) => ({ tensorNetwork: updater(state.tensorNetwork) }));
+  }
+}));
