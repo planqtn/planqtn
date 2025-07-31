280e6a129409fab30154afd3be99aabafe863ec2
Balint Pato <patobalint@gmail.com>
2025-07-30 21:01:14 -0400

    single lego highlights from pcm panel seems to work

---
+++ app/ui/src/stores/operatorHighlightSlice.ts
@@ -0,0 +1,120 @@
+import { StateCreator } from "zustand";
+import { DroppedLego } from "./droppedLegoStore";
+import { PauliOperator } from "../lib/types";
+import { simpleAutoFlow } from "../transformations/AutoPauliFlow";
+
+export interface OperatorHighlightSlice {
+  // Single lego matrix operations
+  handleSingleLegoMatrixRowSelection: (
+    lego: DroppedLego,
+    newSelectedRows: number[]
+  ) => void;
+  handleSingleLegoMatrixChange: (
+    lego: DroppedLego,
+    newMatrix: number[][]
+  ) => void;
+  calculateSingleLegoHighlightedLegConstraints: (
+    matrix: number[][],
+    selectedRows: number[]
+  ) => { legIndex: number; operator: PauliOperator }[];
+}
+
+export const createOperatorHighlightSlice: StateCreator<
+  any,
+  [["zustand/immer", never]],
+  [],
+  OperatorHighlightSlice
+> = (set, get) => ({
+  handleSingleLegoMatrixRowSelection: (
+    lego: DroppedLego,
+    newSelectedRows: number[]
+  ) => {
+    const updateDroppedLego = get().updateDroppedLego;
+    const droppedLegos = get().droppedLegos;
+    const connections = get().connections;
+    const setDroppedLegos = get().setDroppedLegos;
+
+    const updatedLego = new DroppedLego(
+      lego,
+      lego.logicalPosition,
+      lego.instance_id,
+      { selectedMatrixRows: newSelectedRows }
+    );
+
+    updateDroppedLego(updatedLego.instance_id, updatedLego);
+
+    simpleAutoFlow(
+      updatedLego,
+      droppedLegos.map((l: DroppedLego) =>
+        l.instance_id === updatedLego.instance_id ? updatedLego : l
+      ),
+      connections,
+      setDroppedLegos
+    );
+  },
+
+  handleSingleLegoMatrixChange: (lego: DroppedLego, newMatrix: number[][]) => {
+    const updateDroppedLego = get().updateDroppedLego;
+    const droppedLegos = get().droppedLegos;
+    const connections = get().connections;
+    const setDroppedLegos = get().setDroppedLegos;
+
+    const updatedLego = new DroppedLego(
+      lego,
+      lego.logicalPosition,
+      lego.instance_id,
+      { parity_check_matrix: newMatrix }
+    );
+    updateDroppedLego(updatedLego.instance_id, updatedLego);
+    simpleAutoFlow(
+      updatedLego,
+      droppedLegos.map((l: DroppedLego) =>
+        l.instance_id === updatedLego.instance_id ? updatedLego : l
+      ),
+      connections,
+      setDroppedLegos
+    );
+  },
+
+  calculateSingleLegoHighlightedLegConstraints: (
+    matrix: number[][],
+    selectedRows: number[]
+  ): { legIndex: number; operator: PauliOperator }[] => {
+    const highlightedLegConstraints = [];
+
+    if (selectedRows.length > 0) {
+      const combinedRow = new Array(matrix[0].length).fill(0);
+
+      for (const rowIndex of selectedRows) {
+        matrix[rowIndex].forEach((val, idx) => {
+          combinedRow[idx] = (combinedRow[idx] + val) % 2;
+        });
+      }
+
+      // Convert the combined row to leg constraints
+      for (let leg_index = 0; leg_index < matrix[0].length / 2; leg_index++) {
+        const xPart = combinedRow[leg_index];
+        const zPart = combinedRow[leg_index + matrix[0].length / 2];
+
+        if (xPart === 1 && zPart === 0) {
+          highlightedLegConstraints.push({
+            legIndex: leg_index,
+            operator: PauliOperator.X
+          });
+        } else if (xPart === 0 && zPart === 1) {
+          highlightedLegConstraints.push({
+            legIndex: leg_index,
+            operator: PauliOperator.Z
+          });
+        } else if (xPart === 1 && zPart === 1) {
+          highlightedLegConstraints.push({
+            legIndex: leg_index,
+            operator: PauliOperator.Y
+          });
+        }
+      }
+    }
+
+    return highlightedLegConstraints;
+  }
+});
