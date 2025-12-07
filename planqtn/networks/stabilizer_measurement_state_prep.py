f1f3ad4749624d4a19a0dd1bd80d4ab8682d5ce8
balopat <patobalint@gmail.com>
2025-12-07 08:04:22 -0500

    lint

--- planqtn/networks/stabilizer_measurement_state_prep.py
+++ planqtn/networks/stabilizer_measurement_state_prep.py
@@ -55,7 +55,7 @@ class StabilizerMeasurementStatePrepTN(TensorNetwork):
         checks = []
         check_stoppers = []
         for i in range(r):
-            weight = np.count_nonzero(parity_check_matrix[i])
+            weight = int(np.count_nonzero(parity_check_matrix[i]))
             check = StabilizerCodeTensorEnumerator(
                 h=Legos.z_rep_code(weight + 2),
                 tensor_id=f"check{i}",
